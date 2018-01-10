0a71bb8fe284878f9e6968373b5da8c7ae62e872
Dorian Pery <dorian.pery@gmail.com>
2018-01-10 01:08:32 +0100

    Runner V1.0

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour{
+    public Transform player;
+	public Vector3 offset;
+
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.name == "Ground") transform.position = new Vector3(transform.position.x, transform.position.y, player.position.z);
+		else transform.position = player.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine; 
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    private bool moveRight = false;
+    private bool moveLeft = false;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (moveRight) rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        if (moveLeft) rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+		if (rb.position.y < -1f)
+		{
+			FindObjectOfType<GameManager>().EndGame();
+		}
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            moveRight = true;
+            moveLeft = false;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            moveLeft = true;
+            moveRight = false;
+        }
+        else
+        {
+            moveLeft = false;
+            moveRight = false;
+        }
+    }
+}
